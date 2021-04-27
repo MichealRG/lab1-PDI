6635cafbd6ccf6af78c661f5eba717d453e3c63d
Michał <KrzyzowskiM1998@gmail.com>
2021-04-27 13:44:23 +0200

    ff

---
+++ RssReader/RssReader/RssDisplay.aspx
@@ -0,0 +1,72 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="RssDisplay.aspx.cs" Inherits="RssReader.RssDisplay" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+
+    <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+    <asp:Button ID="Button1" runat="server" Text="Button" OnClick="Button1_Click" />
+
+    <div class="row">
+        <%--<asp:GridView ID="GridView1" runat="server" 
+            EmptyDataText="No data available." CellPadding="4" ForeColor="#333333" GridLines="None" OnPageIndexChanging="gv_PageIndexChanging" AllowPaging="True">
+            <AlternatingRowStyle BackColor="White" />
+            <Columns>
+                <asp:BoundField DataField="Title" 
+                HeaderText="Title" ReadOnly="True"
+                SortExpression="Title" />
+                <asp:BoundField DataField="PubDate" 
+                HeaderText="Publication Date" 
+                ReadOnly="True"/>
+                <asp:BoundField DataField="Description" 
+                HeaderText="Description" ReadOnly="true" />
+            </Columns>
+            <EditRowStyle BackColor="#2461BF" />
+            <FooterStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+            <HeaderStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+            <PagerStyle BackColor="#2461BF" ForeColor="White" HorizontalAlign="Center" />
+            <RowStyle BackColor="#EFF3FB" />
+            <SelectedRowStyle BackColor="#D1DDF1" Font-Bold="True" ForeColor="#333333" />
+            <SortedAscen    dingCellStyle BackColor="#F5F7FB" />
+            <SortedAscendingHeaderStyle BackColor="#6D95E1" />
+            <SortedDescendingCellStyle BackColor="#E9EBEF" />
+            <SortedDescendingHeaderStyle BackColor="#4870BE" />
+        </asp:GridView>--%>
+
+
+        <asp:Repeater ID="Repeater1" runat="server" ItemType="RssReader.Model.RssItem" OnItemCommand="rptPaging_ItemCommand">
+            <ItemTemplate>
+                <div class="col-md-6">
+                    <h2>
+                        <%# DataBinder.Eval(Container.DataItem,"Title") %>
+                        <%--<%# Item.Title %>--%>
+                    </h2>
+                    <span class="btn btn-info">
+                        <%# Item.PubDate %>
+                    </span>
+                    <p>
+                        <%# Item.Description %>
+                    </p>
+                </div>
+                <asp:LinkButton ID="btnPage"
+                    Style="padding: 8px; margin: 2px; background: #ffa100; border: solid 1px #666; font: 8pt tahoma;"
+                    CommandName="Page" CommandArgument="<%# Container.DataItem %>"
+                    runat="server" ForeColor="White" Font-Bold="True">
+                 <%# Container.DataItem %>
+                      </asp:LinkButton>
+            </ItemTemplate>
+        </asp:Repeater>
+
+
+        <nav aria-label="Page navigation example">
+            <ul class="pagination">
+                <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+                <li class="page-item"><a class="page-link" href="#">1</a></li>
+                <li class="page-item"><a class="page-link" href="#">2</a></li>
+                <li class="page-item"><a class="page-link" href="#">3</a></li>
+                <li class="page-item"><a class="page-link" href="#">Next</a></li>
+            </ul>
+        </nav>
+        <asp:XmlDataSource ID="XmlDataSource1" runat="server" XPath="https://news.google.com/rss?hl=en-US&amp;gl=US&amp;ceid=US:en"></asp:XmlDataSource>
+    </div>
+
+
+</asp:Content>
---
+++ RssReader/RssReader/RssDisplay.aspx.cs
@@ -0,0 +1,122 @@
+﻿using RssReader.Model;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml.Linq;
+
+namespace RssReader
+{
+    public partial class RssDisplay : System.Web.UI.Page
+    {
+        static List<List<RssItem>> pageRssItems = new List<List<RssItem>>();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var xml = XElement.Load("https://news.google.com/rss?hl=en-US&gl=US&ceid=US:en");
+            //var items = from n in xml.Descendants("item")
+            //            select new RssItem
+            //            {
+            //                Title = n.Element("title").Value,
+            //                Description= n.Element("description").Value,
+            //                PubDate = n.Element("pubDate").Value,
+            //            };
+            var items = xml.Descendants("item").Select(x => new RssItem
+            {
+                Title = x.Element("title").Value,
+                Description = x.Element("description").Value,
+                PubDate = x.Element("pubDate").Value,
+            });
+            for (int i = 0; i < 4; i++)
+            {
+                pageRssItems.Add(new List<RssItem>());
+            }
+            for (int i = 0; i < items.Count(); i+=4)
+            {
+                if (i == items.Count() || items.Count() == i + 1 || items.Count() == i + 2 || items.Count() == i + 3)
+                    break;
+                pageRssItems[0].Add(items.ElementAt(i));
+                pageRssItems[1].Add(items.ElementAt(i+1));
+                pageRssItems[2].Add(items.ElementAt(i+2));
+                pageRssItems[3].Add(items.ElementAt(i+3));
+            }
+
+            //Create the PagedDataSource that will be used in paging
+            PagedDataSource pgitems = new PagedDataSource();
+            pgitems.DataSource = items;
+            
+            pgitems.AllowPaging = true;
+
+            //Control page size from here 
+            pgitems.PageSize = 4;
+            pgitems.CurrentPageIndex = PageNumber;
+            if (pgitems.PageCount > 1)
+            {
+                Repeater1.Visible = true;
+                ArrayList pages = new ArrayList();
+                for (int i = 0; i <= pgitems.PageCount - 1; i++)
+                {
+                    pages.Add((i + 1).ToString());
+                }
+                Repeater1.DataSource = pages;
+                Repeater1.DataBind();
+            }
+            else
+            {
+                Repeater1.Visible = false;
+            }
+
+            //Finally, set the datasource of the repeater
+            Repeater1.DataSource = pgitems;
+            Repeater1.DataBind();
+
+            //GridView1.DataSource = pageRssItems[0];
+            //GridView1.DataBind();
+            //Repeater1.DataSource = items;
+            //Repeater1.DataBind();//przypiecie danych
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            var link = TextBox1.Text;
+            var xml = XElement.Load(link);
+            var items = xml.Descendants("item").Select(x => new RssItem
+            {
+                Title = x.Element("title").Value,
+                Description = x.Element("description").Value,
+                PubDate = x.Element("pubDate").Value,
+            });
+            //GridView1.DataSource= items;
+            //GridView1.DataBind();
+            //Repeater1.DataSource = items;
+            //Repeater1.DataBind();//przypiecie danych
+        }
+        public int PageNumber
+        {
+            get
+            {
+                if (ViewState["PageNumber"] != null)
+                {
+                    return Convert.ToInt32(ViewState["PageNumber"]);
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            set { ViewState["PageNumber"] = value; }
+        }
+        protected void rptPaging_ItemCommand(object source, System.Web.UI.WebControls.RepeaterCommandEventArgs e)
+        {
+            PageNumber = Convert.ToInt32(e.CommandArgument) - 1;
+            Page_Load(source,e);
+        }
+        //protected void gv_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        //{
+        //    GridView1.PageIndex = e.NewPageIndex;
+        //    GridView1.DataBind();
+        //}
+    }
+}
